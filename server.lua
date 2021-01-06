0c2700408e2f9b1f6b68c688993cc7713157eb84
Ezague <kristiansanderpetersen@gmail.com>
2021-01-06 19:50:10 +0100

    Updated FiveM Script

--- server.lua
+++ server.lua
@@ -1,34 +1,30 @@
 local function OnPlayerConnecting(name, setKickReason, deferrals)
     local player = source
-    local steamIdentifier
     local identifiers = GetPlayerIdentifiers(player)
+
+    local steamid  = false
+    local license  = false
+    local discord  = false
+    local xbl      = false
+    local liveid   = false
+    found          = false
     deferrals.defer()
 
     Wait(0)
+    deferrals.update("Finder identifiers...")
 
-    deferrals.update("Forbinder til Steam...")
-
-    for _, v in pairs(identifiers) do
-        if string.find(v, "steam") then
-            steamIdentifier = v
-            break
-        end
-    end
-
-    Wait(0)
-
-    if not steamIdentifier then
-        deferrals.done("[FiveM Modder Advarsel] Du er ikke forbundet til Steam")
-    else
-        PerformHttpRequest("http://64.225.105.125:8081/verify=" .. steamIdentifier, function(err, text, headers)
-            if text == "false" then
-                print("[FiveM Modder Advarsel] " .. steamIdentifier .. " udelukket")
-                deferrals.done("[FiveM Modder Advarsel] Du er udelukket fra denne server")
-            else
-                deferrals.done()
+    for k,v in pairs(identifiers)do
+        PerformHttpRequest("https://fivem.dk/defender/single/" .. v, function(err, text, headers)
+            if text == '{"status":"found"}' then
+                found = true
+                print("[FiveM Defender] " .. v .. " excluded due to confirmed modding.")
+                deferrals.done("[FiveM Defender] You are excluded from this server due to modding.")
             end
         end, 'GET', '')
     end
+
+    Wait(1000)
+    if not found then deferrals.done() end
 end
 
 AddEventHandler("playerConnecting", OnPlayerConnecting)
