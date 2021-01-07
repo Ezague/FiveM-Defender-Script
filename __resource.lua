10d056e79e390738736f63365679ce14358d6ecb
OssieFromDK <50819244+OssieFromDK@users.noreply.github.com>
2021-01-07 11:59:25 +0100

    manifest updates

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,6 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
-server_script 'server.lua'
+server_scripts {
+    'server.lua',
+    'config.lua'
+}
--- fxmani.lua
+++ fxmani.lua
@@ -1,3 +1,6 @@
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
-server_script 'server.lua'
+server_scripts {
+    'server.lua',
+    'config.lua'
+}
