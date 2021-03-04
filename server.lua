a9ffdfea2b6d724bf1ec2d76853a5876041a27b5
Sh4dow8080 <78806525+Sh4dow8080@users.noreply.github.com>
2021-03-04 19:39:44 +0100

    Added license2, Optimized a little

--- server.lua
+++ server.lua
@@ -27,68 +27,6 @@ function log(identifier, message)
     end
 end
 
-Citizen.CreateThread(function()
-    if (GetCurrentResourceName() ~= resourceName) then
-        PerformHttpRequest("https://raw.githubusercontent.com/Ezague/FiveM-Defender-Script/main/version.txt", function(err, text, headers)
-            if text == version then
-                print("[FiveM Defender] The script is up to date")
-            else
-                print("^1[FiveM Defender] OUTDATED - Download newest version from: https://github.com/Ezague/FiveM-Defender-Script^1")
-            end
-        end, 'GET', '')
-    end
-
-    PerformHttpRequest("https://fivem.dk/defender/all", function(statusCode, text, headers)
-        if statusCode == 200 or statusCode == 304 then
-            if text ~= nil and text ~= "" then
-                for i,k in pairs(json.decode(text)) do
-                    for x,b in pairs(k) do
-                        if x == "steam_id" or x == "xbox_id" or x == "live_id" or x == "license_id" or x == "discord_id" then
-                            if b ~= "null" and b ~= nil then
-                                table.insert(cachedLicenses, b)
-                            end
-                        end
-                    end
-                end
-                print("[FiveM Defender] Cache has been loaded.")
-            else
-                print("[FiveM Defender] Failed to load cache.")
-            end
-        else
-            print("[FiveM Defender] Failed to load cache.")
-        end
-    end, 'GET', '')
-end)
-
-local function OnPlayerConnecting(name, setKickReason, deferrals)
-    local player = source
-    local identifiers = GetPlayerIdentifiers(player)
-    local found          = false
-
-    deferrals.defer()
-
-    Wait(0)
-    deferrals.update("[FiveM Defender] Checking identifiers...")
-
-    for k,v in pairs(identifiers) do
-        for i,identifier in pairs(cachedLicenses) do
-            if v == identifier then
-                if not checkBypass(v) then
-                    found = true
-                    log(v, "User was excluded due to confirmed modding.")
-                    deferrals.done("[FiveM Defender] You are excluded from this server due to modding. \nFiveM Defender: discord.gg/MJvp3w7d4t")
-                else
-                    log(v, "User was a modder, but was allowed access to the server because you set them up in your bypass.")
-                end
-                break;
-            end
-        end
-    end
-    Wait(1000)
-    if not found then deferrals.done() end
-end
-AddEventHandler("playerConnecting", OnPlayerConnecting)
-
 function checkBypass(identifier)
     for k,v in pairs(Config.Bypass) do
         if v == identifier then
@@ -97,3 +35,59 @@ function checkBypass(identifier)
     end
     return false
 end
+
+
+Citizen.CreateThread(function()
+    PerformHttpRequest("https://raw.githubusercontent.com/Ezague/FiveM-Defender-Script/main/version.txt", function(err, text, headers)
+        if text == version then
+            print("[FiveM Defender] The script is up to date")
+        else
+            print("^1[FiveM Defender] OUTDATED - Download newest version from: https://github.com/Ezague/FiveM-Defender-Script^0")
+        end
+    end, 'GET', '')
+
+    PerformHttpRequest("https://fivem.dk/defender/all", function(statusCode, text, headers)
+        if statusCode == 200 or statusCode == 304 then
+            if text ~= nil and text ~= "" then
+                for i,k in pairs(json.decode(text)) do
+                    for x,b in pairs(k) do
+                        if b ~= "null" and b ~= nil then
+                            cachedLicenses[b] = true
+                        end
+                    end
+                end
+                print("^2[FiveM Defender] Cache has been loaded.^0")
+            else
+                print("^1[FiveM Defender] Failed to load cache, FiveM Defender will be disabled.^0")
+            end
+        else
+            print("[FiveM Defender] Failed to load cache, FiveM Defender will be disabled.^0")
+        end
+    end, 'GET', '')
+end)
+
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local player = source
+    local identifiers = GetPlayerIdentifiers(player)
+    local found = false
+    deferrals.defer()
+
+    Wait(10)
+
+    deferrals.update("[FiveM Defender] Checking identifiers...")
+
+    for k,v in pairs(identifiers) do
+        if cachedLicenses[v] == true then
+            if not checkBypass(v) then
+                found = true
+                log(v, "User was excluded due to confirmed modding.")
+                deferrals.done("\n[FiveM Defender] You are excluded from this server due to modding.\n[Discord] discord.gg/MJvp3w7d4t")
+            else
+                log(v, "User was a modder, but was allowed access to the server because you set them up in your bypass.")
+            end
+            break;
+        end
+    end
+    Wait(1000)
+    if not found then deferrals.done() end
+end)
